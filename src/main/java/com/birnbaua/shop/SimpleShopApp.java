4e0934c98a89a75f0ca177c28ba04e6ece31a345
Birnbauaa <andreas.bachl@24speed.at>
2020-10-17 22:01:25 +0200

    make deployable on tomcat

--- src/main/java/com/birnbaua/shop/SimpleShopApp.java
+++ src/main/java/com/birnbaua/shop/SimpleShopApp.java
@@ -2,10 +2,17 @@ package com.birnbaua.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleShopApp {
+public class SimpleShopApp extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SimpleShopApp.class);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(SimpleShopApp.class, args);
     }
